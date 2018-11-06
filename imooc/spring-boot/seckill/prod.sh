a679a23299919565350789886758c0ec81c71bae
Tingkl <dingguoliang01@baidu.com>
2018-11-06 18:05:53 +0800

    s

---
+++ imooc/spring-boot/seckill/prod.sh
@@ -0,0 +1,7 @@
+mvn clean package
+
+cp -r ./target/lib /Users/tingkl/Desktop/prod/ms
+
+cp ./target/miaosha.jar /Users/tingkl/Desktop/prod/ms
+
+cp ./src/main/resources/tingkl.conf /Users/tingkl/Desktop/prod/ms
