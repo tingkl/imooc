642397b061c9f12d527fcee56ae5fe04ee473247
Tingkl <dingguoliang01@baidu.com>
2018-11-06 18:53:07 +0800

    s

--- imooc/spring-boot/seckill/prod.sh
+++ imooc/spring-boot/seckill/prod.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 mvn clean package
 
 cp -r ./target/lib /Users/tingkl/Desktop/prod/ms
@@ -5,3 +6,11 @@ cp -r ./target/lib /Users/tingkl/Desktop/prod/ms
 cp ./target/miaosha.jar /Users/tingkl/Desktop/prod/ms
 
 cp ./src/main/resources/tingkl.conf /Users/tingkl/Desktop/prod/ms
+
+cd /Users/tingkl/Desktop/prod/ms
+
+git add *
+
+git commit -m 'auto'
+
+git push
